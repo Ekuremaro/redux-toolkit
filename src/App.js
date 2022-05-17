e198ae85d5af86b2be0b626ee31b0a18d4d7f158
Ekuremaro <86929331+Ekuremaro@users.noreply.github.com>
2022-05-17 02:29:25 +0100

    createasyncthunk

--- src/App.js
+++ src/App.js
@@ -1,4 +1,36 @@
+import Navbar from './components/Navbar';
+import { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import CartContainer from './components/CartContainer';
+import { calculateTotals, getCartItems } from './features/cart/cartSlice';
+import Modal from './components/Modal';
 function App() {
-  return <h2>Redux Toolkit</h2>;
+  const dispatch = useDispatch();
+  const { cartItems, isLoading } = useSelector((state) => state.cart);
+  const { isOpen } = useSelector((state) => state.modal);
+
+  useEffect(() => {
+    dispatch(calculateTotals());
+  }, [cartItems]);
+
+  useEffect(() => {
+    dispatch(getCartItems());
+  }, []);
+
+  if (isLoading) {
+    return (
+      <div className="loading">
+        <h1>Loading...</h1>
+      </div>
+    );
+  }
+
+  return (
+    <main>
+      {isOpen && <Modal />}
+      <Navbar />
+      <CartContainer />
+    </main>
+  );
 }
 export default App;
